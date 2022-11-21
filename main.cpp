e4dafec759fc3d596574e869738c497017e79835
day253 <9634619+day253@users.noreply.github.com>
2022-11-21 07:37:46 +0000

    init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "marisa.h"
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <system_error>
+
+int main(int argc, char** argv) {
+    const auto& trie = marisa::Trie();
+    return 0;
+}
