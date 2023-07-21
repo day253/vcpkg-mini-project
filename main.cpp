449245f2a89a93055cbd64f4786a08352f88f959
day253 <9634619+day253@users.noreply.github.com>
2023-07-22 00:25:38 +0800

    mini

--- main.cpp
+++ main.cpp
@@ -1,11 +1,5 @@
-#include "marisa.h"
-
-#include <filesystem>
-#include <fstream>
 #include <iostream>
-#include <system_error>
 
 int main(int argc, char** argv) {
-    const auto& trie = marisa::Trie();
     return 0;
 }
